dc7ec5c9955ddb9c4ed0263f9d1c0aa742d49fd8
Nieuwlaar <nieuwlaar@gmail.com>
2022-04-18 11:41:45 +0200

    fix for passing tests on github attempt 1

--- valuetransfer/src/androidTest/java/nl/tudelft/trustchain/valuetransfer/ui/exchangelink/ExchangeTransferMoneyLinkFragmentTest.kt
+++ valuetransfer/src/androidTest/java/nl/tudelft/trustchain/valuetransfer/ui/exchangelink/ExchangeTransferMoneyLinkFragmentTest.kt
@@ -1,9 +1,5 @@
 package nl.tudelft.trustchain.valuetransfer.ui.exchangelink
 
-import nl.tudelft.trustchain.valuetransfer.dialogs.ExchangeTransferMoneyLinkDialog
-import org.junit.Assert.*
-import org.junit.Test
-
 //class ExchangeTransferMoneyLinkFragmentTest {
 //}
 
